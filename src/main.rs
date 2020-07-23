a875bd6f7837151e3a11a41ce5d0ce000394016d
Oliver Daff <oliverdaff@users.noreply.github.com>
2020-07-23 21:33:05 +1000

    read from file and throttle requests per second

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use clap::{App, Arg};
+use futures::prelude::*;
+use futures::stream;
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
 use std::path::Path;
+use std::time::Duration;
+use stream_throttle::{ThrottlePool, ThrottleRate, ThrottledStream};
 use trust_dns_resolver::config::*;
 use trust_dns_resolver::TokioAsyncResolver;
 
@@ -26,18 +32,34 @@ async fn main() {
         )
         .get_matches();
 
-    let domain = command.value_of("DOMAIN").unwrap();
+    let domain = command.value_of("DOMAIN").expect("domain expected");
+    let subdomains_file = command.value_of("SUBDOMAINS").expect("subdomains expected");
+
+    let file = File::open(subdomains_file).expect("Could not open file");
+    let reader = BufReader::new(file);
+
+    let rate = ThrottleRate::new(100, Duration::from_secs(1));
+    let pool = ThrottlePool::new(rate);
 
     let resolver = TokioAsyncResolver::tokio(ResolverConfig::default(), ResolverOpts::default());
 
     let res = resolver.await.expect("Failed to connect to resolver");
-    let response = res
-        .lookup_ip(domain)
-        .await
-        .expect("Failed to resolve address");
-    response.iter().for_each(|x| {
-        println!("{:?}", x);
-    })
+
+    let stream = stream::iter(reader.lines())
+        .throttle(pool)
+        .map_err(|e| format!("error {}", e))
+        .and_then(|prefix| {
+            res.lookup_ip(format!("{}.{}", prefix, domain))
+                .map_err(|e| format!("error: {}", e))
+        })
+        .filter(|x| future::ready(x.is_ok()))
+        .for_each(|x| {
+            if let Ok(r) = x {
+                println!("{:?}", r);
+            }
+            future::ready(())
+        });
+    stream.await;
 }
 
 fn validate_subdomain_file(file: String) -> Result<(), String> {
