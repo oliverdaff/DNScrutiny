1e78e98fe85df36dbf2a38277de069a03a91565d
Oliver Daff <oliverdaff@users.noreply.github.com>
2020-07-30 21:11:14 +1000

    check for Wildcard domains before running the brute force

--- src/brute.rs
+++ src/brute.rs
@@ -1,5 +1,7 @@
 use futures::prelude::*;
 use futures::stream;
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use stream_throttle::{ThrottlePool, ThrottledStream};
@@ -12,6 +14,16 @@ pub async fn brute_force_domain(
     throttle_pool: ThrottlePool,
     resolver: &TokioAsyncResolver,
 ) -> Vec<Record> {
+    if let Ok(result) = is_wildcard_domain(domain, resolver).await {
+        println!("Wild card resolution is enabled on this domain");
+        println!("All domains will resolve to these ips");
+        return result
+            .as_lookup()
+            .record_iter()
+            .cloned()
+            .collect::<Vec<_>>();
+    }
+
     let file = File::open(subdomains_file).expect("Could not open file");
     let reader = BufReader::new(file);
 
@@ -32,4 +44,14 @@ pub async fn brute_force_domain(
         .iter()
         .flat_map(|x| x.record_iter().cloned())
         .collect::<Vec<_>>()
+}
+
+/// Check if domain is configured with Wildcard resolution
+/// by requesting a random address
+async fn is_wildcard_domain(
+    domain: &str,
+    resolver: &TokioAsyncResolver,
+) -> Result<trust_dns_resolver::lookup_ip::LookupIp, trust_dns_resolver::error::ResolveError> {
+    let prefix: String = thread_rng().sample_iter(&Alphanumeric).take(12).collect();
+    resolver.lookup_ip(format!("{}.{}", prefix, domain)).await
 }
